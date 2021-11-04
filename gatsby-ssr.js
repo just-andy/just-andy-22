6428969eb43b71851f2b00a1cc2cd63a72c88fd7
Andys Macbook Pro <andy@just-andy.uk>
2021-11-04 22:08:53 +0000

    Happy with type, fixed about, some other progress

--- gatsby-ssr.js
+++ gatsby-ssr.js
@@ -1,9 +1,10 @@
-/* eslint-disable react/jsx-props-no-spreading */
 import React from 'react';
 import Layout from './src/components/Layout';
 import './src/styles/global.css';
-import "@fontsource/ostrich-sans/900.css"
-require("prismjs/themes/prism-dark.css")
+import "@fontsource/league-spartan/700.css";
+import "@fontsource/league-spartan/400.css";
+import "@fontsource/Baskervville/400.css";
+require("prismjs/themes/prism-dark.css");
 
 export function wrapPageElement({ element, props }) {
     return <Layout {...props}>{element}</Layout>;
