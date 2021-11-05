88332896a037d45d24128756a526ec13f902ec65
Andys Macbook Pro <andy@just-andy.uk>
2021-11-05 09:30:30 +0000

    fixed typo and updated gatsby netlify plugin

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -1,9 +1,8 @@
 import React from 'react';
 import Layout from './src/components/Layout';
 import './src/styles/global.css';
-require("@fontsource/league-spartan/700.css");
-require("@fontsource/league-spartan/400.css");
-require("@fontsource/Baskervville/400.css");
+import "@fontsource/league-spartan";
+import "@fontsource/baskervville";
 require("prismjs/themes/prism-dark.css");
 
 export function wrapPageElement({ element, props }) {
