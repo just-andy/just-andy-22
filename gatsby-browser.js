d5c0ca6380496a3b87ffe881a71489a90b0a83ba
Andys Macbook Pro <andy@just-andy.uk>
2021-11-05 08:48:11 +0000

    changed imports to require

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -1,9 +1,9 @@
 import React from 'react';
 import Layout from './src/components/Layout';
 import './src/styles/global.css';
-import "@fontsource/league-spartan/700.css";
-import "@fontsource/league-spartan/400.css";
-import "@fontsource/Baskervville/400.css";
+require("@fontsource/league-spartan/700.css");
+require("@fontsource/league-spartan/400.css");
+require("@fontsource/Baskervville/400.css");
 require("prismjs/themes/prism-dark.css");
 
 export function wrapPageElement({ element, props }) {
